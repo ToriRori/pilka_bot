89f2184eee80c364b6eb2b5b1c78be3e16a57518
ToriRori <torirorik@gmail.com>
2020-06-05 18:04:50 +1100

    add message handlers

--- main.py
+++ main.py
@@ -4,78 +4,210 @@ import time
 import config
 import telebot
 from telebot import apihelper, types
-
+from collections import defaultdict
 import utils
 from SQLighter import SQLighter
 
+# inline keyboard for buttons in chat
 apihelper.proxy = {
-    'https':'socks5://217.182.230.15:4485'
+    'https': 'socks5://217.182.230.15:4485'
 }
+
+START, REPEAT, REPEAT_TYPE, REPEAT_DURATION, DATE, DURATION, CONFIRMATION, GETMASTER, GETCLIENT, DELETECLIENT, DELETEMASTER= range(11)
 
 bot = telebot.TeleBot(config.token, threaded=False)
 
-# @bot.message_handler(commands=['test'])
-# def find_file_ids(message):
-#     for file in os.listdir('music/'):
-#         if file.split('.')[-1] == 'ogg':
-#             f = open('music/'+file, 'rb')
-#             msg = bot.send_voice(message.chat.id, f, None)
-#             # А теперь отправим вслед за файлом его file_id
-#             bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
-#         time.sleep(3)
+USER_STATE = defaultdict(lambda: START)
 
-# @bot.message_handler(commands=['game'])
-# def game(message):
-#     # Подключаемся к БД
-#     db_worker = SQLighter(config.database_name)
-#     # Получаем случайную строку из БД
-#     row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
-#     # Формируем разметку
-#     markup = utils.generate_markup(row[2], row[3])
-#     # Отправляем аудиофайл с вариантами ответа
-#     bot.send_voice(message.chat.id, row[1], reply_markup=markup)
-#     # Включаем "игровой режим"
-#     utils.set_user_game(message.chat.id, row[2])
-#     # Отсоединяемся от БД
-#     db_worker.close()
 
+def get_state(message):
+    return USER_STATE[message.chat.id]
+
+
+def update_state(message, state):
+    USER_STATE[message.chat.id] = state
+
+
+@bot.message_handler(commands=['start'])
+def get_schedule(message):
+    # Формируем разметку
+    bot.send_message(message.chat.id, 'Добро пожаловать в бот!')
+
+####################################################
+# get schedule for client and master
 @bot.message_handler(commands=['get_schedule'])
 def get_schedule(message):
-    # Формируем разметку
     markup = utils.generate_markup_to_get_schedule()
     bot.send_message(message.chat.id, 'Выбери сервис', reply_markup=markup)
+    if message.from_user.username == config.admin:
+        update_state(message, GETMASTER)
+    else:
+        update_state(message, GETCLIENT)
 
+
+@bot.message_handler(func=lambda message: get_state(message) == GETCLIENT)
+def choose_type_repeat(message):
+    if message.text == 'Расписание на сегодня':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на завтра':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на неделю':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на месяц':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    list = ['event1', 'event2', 'event3']
+    keyboard = types.InlineKeyboardMarkup()
+    for item in list:
+        callback_button = types.InlineKeyboardButton(text=item, callback_data=item)
+        keyboard.add(callback_button)
+    bot.send_message(message.chat.id, "Свободные окна", reply_markup=keyboard)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == GETMASTER)
+def choose_type_repeat(message):
+    if message.text == 'Расписание на сегодня':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на завтра':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на неделю':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    if message.text == 'Расписание на месяц':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+    bot.send_message(message.chat.id, message.text, reply_markup=keyboard_hider)
+    list = ['event3', 'event2', 'event1']
+    keyboard = types.InlineKeyboardMarkup()
+    for item in list:
+        callback_button = types.InlineKeyboardButton(text=item, callback_data=item)
+        keyboard.add(callback_button)
+    bot.send_message(message.chat.id, "Ваши размещенные сеансы", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    #if call.message:
+    markup = utils.generate_markup_agree()
+    bot.send_message(call.message.chat.id, 'Хотите удалить событие '+call.data+'?', reply_markup=markup)
+    if get_state(call.message) == GETCLIENT:
+        update_state(call.message, DELETECLIENT)
+    if get_state(call.message) == GETMASTER:
+        update_state(call.message, DELETEMASTER)
+
+@bot.message_handler(func=lambda message: get_state(message) == DELETECLIENT)
+def choose_type_repeat(message):
+    if message.text == 'Да':
+        keyboard_hider = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Сеанс успешно отменен', reply_markup=keyboard_hider)
+        #отправить уведомление мастеру
+
+    if message.text == 'Нет':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+@bot.message_handler(func=lambda message: get_state(message) == DELETEMASTER)
+def choose_type_repeat(message):
+    if message.text == 'Да':
+        keyboard_hider = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Сеанс успешно отменен', reply_markup=keyboard_hider)
+        #проверка свободно ли, если нет, отправить уведомление клиенту
+
+    if message.text == 'Нет':
+        keyboard_hider = types.ReplyKeyboardRemove()
+
+####################################################
+# show reserved events
+# @bot.message_handler(commands=['show_reserved'])
+# def get_schedule(message):
+
+
+####################################################
+# show reserved events
+# @bot.message_handler(commands=['show_applications'])
+# def get_schedule(message):
+
+
+####################################################
+# put master's event
 @bot.message_handler(commands=['put_schedule'])
 def get_schedule(message):
-    # Формируем разметку
     markup = utils.generate_markup_to_put_schedule()
     bot.send_message(message.chat.id, 'Выбери сервис', reply_markup=markup)
+    update_state(message, REPEAT)
 
 
+@bot.message_handler(func=lambda message: get_state(message) == REPEAT)
+def choose_type_repeat(message):
+    if message.text == 'Одиночное окно':
+        keyboard_hider = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Напиши дату и время начала окна', reply_markup=keyboard_hider)
+        update_state(message, DATE)
+    if message.text == 'Повторяющееся окно':
+        markup = utils.generate_markup_to_get_type_of_repeat()
+        bot.send_message(message.chat.id, 'Выбери как будет повторяться окно', reply_markup=markup)
+        update_state(message, REPEAT_TYPE)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == REPEAT_TYPE)
+def choose_type_repeat(message):
+    # if valid message
+    markup = utils.generate_markup_to_get_duration_of_repeat()
+    bot.send_message(message.chat.id, 'Выбери как долго окно будет повторяться', reply_markup=markup)
+    update_state(message, REPEAT_DURATION)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == REPEAT_DURATION)
+def choose_type_repeat(message):
+    # if valid message
+    keyboard_hider = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, 'Напиши дату и время начала окна', reply_markup=keyboard_hider)
+    update_state(message, DATE)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == DATE)
+def choose_type_repeat(message):
+    # if valid message
+    bot.send_message(message.chat.id, 'Напиши длительность окна в минутах')
+    update_state(message, DURATION)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == DURATION)
+def choose_type_repeat(message):
+    # if valid message
+    markup = utils.generate_markup_agree()
+    bot.send_message(message.chat.id, 'Добавить такое-то событие?', reply_markup=markup)
+    update_state(message, CONFIRMATION)
+
+
+@bot.message_handler(func=lambda message: get_state(message) == CONFIRMATION)
+def choose_type_repeat(message):
+    # if valid message
+    keyboard_hider = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, 'Окно успешно добавлено', reply_markup=keyboard_hider)
+    update_state(message, START)
+
+
+####################################################
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def check_answer(message):
-    # Если функция возвращает None -> Человек не в игре
-    # answer = utils.get_answer_for_user(message.chat.id)
-    # # Как Вы помните, answer может быть либо текст, либо None
-    # # Если None:
-    # if not answer:
-    #     bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
-    # else:
-        # Уберем клавиатуру с вариантами ответа.
-    keyboard_hider = types.ReplyKeyboardRemove()
-    bot.send_message(message.chat.id, 'Так точно!', reply_markup=keyboard_hider)
-        # Если ответ правильный/неправильный
-        # if message.text == answer:
-        #     bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
-        # else:
-        #     bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!', reply_markup=keyboard_hider)
-        # # Удаляем юзера из хранилища (игра закончена)
-        # utils.finish_user_game(message.chat.id)
+    answer = message
+    print(answer)
+    if not answer:
+        bot.send_message(message.chat.id, 'Чтобы начать выберите команду /getschedule /putschedule')
+    else:
+        keyboard_hider = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, 'Так точно!', reply_markup=keyboard_hider)
+
 
 if __name__ == '__main__':
-    while (True):
+    print('start')
+    while True:
         try:
-            utils.count_rows()
+            # utils.count_rows()
             random.seed()
             bot.polling(none_stop=True)
         except:
--- utils.py
+++ utils.py
@@ -17,55 +17,38 @@ from config import shelve_name, database_name
 #     with shelve.open(shelve_name) as storage:
 #         rowsnum = storage['rows_count']
 #     return rowsnum
-#
-#
-# def set_user_game(chat_id, estimated_answer):
-#     with shelve.open(shelve_name) as storage:
-#         storage[str(chat_id)] = estimated_answer
-#
-#
-# def finish_user_game(chat_id):
-#     with shelve.open(shelve_name) as storage:
-#         del storage[str(chat_id)]
-#
-#
-# def get_answer_for_user(chat_id):
-#     with shelve.open(shelve_name) as storage:
-#         try:
-#             answer = storage[str(chat_id)]
-#             return answer
-#         except KeyError:
-#             return None
-#
-#
-# def generate_markup(right_answer, wrong_answers):
-#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-#     # Склеиваем правильный ответ с неправильными
-#     all_answers = '{},{}'.format(right_answer, wrong_answers)
-#     # Создаем лист (массив) и записываем в него все элементы
-#     list_items = []
-#     for item in all_answers.split(','):
-#         list_items.append(item)
-#     # Хорошенько перемешаем все элементы
-#     shuffle(list_items)
-#     # Заполняем разметку перемешанными элементами
-#     for item in list_items:
-#         markup.add(item)
-#     return markup
 
+
+def get_answer_for_user(chat_id):
+    with shelve.open(shelve_name) as storage:
+        try:
+            answer = storage[str(chat_id)]
+            return answer
+        except KeyError:
+            return None
 
 def generate_markup_to_get_schedule():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     list_items = ['Расписание на сегодня', 'Расписание на завтра', 'Расписание на неделю', 'Расписание на месяц']
-    # Заполняем разметку перемешанными элементами
-    for item in list_items:
-        markup.add(item)
-    return markup
+    return generate_markup(list_items)
 
 def generate_markup_to_put_schedule():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     list_items = ['Одиночное окно', 'Повторяющееся окно']
-    # Заполняем разметку перемешанными элементами
+    return generate_markup(list_items)
+
+def generate_markup_to_get_type_of_repeat():
+    list_items = ['Каждый день (без выходных)', 'Каждый день (с выходными сб, вс)', 'Каждую неделю']
+    return generate_markup(list_items)
+
+def generate_markup_to_get_duration_of_repeat():
+    list_items = ['Неделю', 'До конца месяца', 'Месяц']
+    return generate_markup(list_items)
+
+def generate_markup_agree():
+    list_items = ['Да', 'Нет']
+    return generate_markup(list_items)
+
+def generate_markup(list_items):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     for item in list_items:
         markup.add(item)
     return markup
